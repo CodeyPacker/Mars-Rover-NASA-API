8e1aba989d4f38cac1e847bacaa8367b78158e69
Cody Packer <codeypacker@gmail.com>
2021-02-15 01:11:21 -0700

    Updated API fetch to include 'Curiosity

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -78,28 +78,32 @@ const ImageOfTheDay = (apod) => {
 
     // check if the photo of the day is actually type video!
     if (apod.media_type === "video") {
-        return (`
-            <p>See today's featured video <a href="${apod.url}">here</a></p>
-            <p>${apod.title}</p>
-            <p>${apod.explanation}</p>
-        `)
+        // return (`
+        //     <p>See today's featured video <a href="${apod.url}">here</a></p>
+        //     <p>${apod.title}</p>
+        //     <p>${apod.explanation}</p>
+        // `)
     } else {
-        return (`
-            <img src="${apod.image.url}" height="350px" width="100%" />
-            <p>${apod.image.explanation}</p>
-        `)
+        // return (`
+        //     <img src="${apod.image.url}" height="350px" width="100%" />
+        //     <p>${apod.image.explanation}</p>
+        // `)
     }
 }
 
 // ------------------------------------------------------  API CALLS
 
 // Example API call
-const getImageOfTheDay = (state) => {
+// Gets data from /apod
+const getImageOfTheDay = async (state) => {
     let { apod } = state
 
-    fetch(`http://localhost:3000/apod`)
+    fetch(`http://localhost:3000/apod`, {
+        headers: {
+            'roverName': 'Curiosity',
+        }
+    })
         .then(res => res.json())
         .then(apod => updateStore(store, { apod }))
-
     return data
 }
--- project/src/server/index.js
+++ project/src/server/index.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const { Map } = require('immutable')
 const express = require('express')
 const bodyParser = require('body-parser')
 const fetch = require('node-fetch')
@@ -15,9 +16,12 @@ app.use('/', express.static(path.join(__dirname, '../public')))
 // your API calls
 
 // example API call
+// Adds data to /apod
 app.get('/apod', async (req, res) => {
+    let roverName = req.get('roverName');
+    console.log("API called for:", roverName);
     try {
-        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
+        let image = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=${process.env.API_KEY}`)
             .then(res => res.json())
         res.send({ image })
     } catch (err) {
