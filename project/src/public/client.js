be8a9410fd7b1567f2e05837106b0161ba044f16
Cody Packer <codeypacker@gmail.com>
2021-02-28 01:39:19 -0700

    Refactored to use Immutable.js

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -1,125 +1,182 @@
-let store = {
-    apod: '',
-    rovers: ['Curiosity', 'Opportunity', 'Spirit'],
-    currentRover: 'curiosity',
-    roverData: ''
-}
+const store = Immutable.Map({
+  currentRover: "curiosity",
+  rovers: ["Curiosity", "Opportunity", "Spirit"],
+});
 
 // add our markup to the page
-const root = document.getElementById('root')
+const root = document.getElementById("root");
 
-const updateStore = (store, newState) => {
-    store = Object.assign(store, newState)
-    render(root, store)
-}
+const updateStore = (store, newState = {}) => {
+  console.log(newState);
+  const newStore = store.mergeDeep(newState);
+  render(root, newStore);
+};
 
 const render = async (root, state) => {
-    root.innerHTML = App(state)
-}
-
+  root.innerHTML = App(state);
+  buttonListeners(state);
+};
 
 // create content
 const App = (state) => {
-    let { apod, roverData } = state
-
-    return `
+  const apod = state.get("image");
+  const currentRover = state.get("currentRover");
+  const roverData = state.get("data");
+  return `
         <header></header>
         <main>
             <section class="hero">
                 <h1>Rover Dashboard</h2>
-                ${ImageOfTheDay(apod)}
+                ${imageOfTheDay(apod)}
             </section>
             <section>
                 <h2>Choose a Rover</h2>
                 <div class="dashboard">
                     <div class="select-rover">
-                        <button value="curiosity">Curiosity</button>
-                        <button value="opportunity">Opportunity</button>
-                        <button value="spirit">Spirit</button>
+                      ${createButtons(state)}
                     </div>
                     <div class="rover-stats">
                         <div class="rover-stats-card">
-                            ${roverStats(roverData)}
+                          ${roverStats(roverData, currentRover)}
                         </div>
                     </div>
                 </div>
             </section>
+            <section class="image-gallery">
+              <h2>Recent Images</h2>
+              <div class="gallery-wrapper">
+                ${roverImages(currentRover, roverData)}
+              </div>
+            </section>
         </main>
         <footer></footer>
-    `
-}
+    `;
+};
 
 // listening for load event because page should load before any JS is called
-window.addEventListener('load', () => {
-    render(root, store)
-})
+window.addEventListener("load", () => {
+  render(root, store);
+  getRoverData((data) => {
+    const rovers = Immutable.Map({ data });
+    updateStore(store, rovers);
+  });
+  getImageOfTheDay((image) => {
+    const apod = Immutable.Map({ image });
+    updateStore(store, apod);
+  });
+});
 
 // ------------------------------------------------------  COMPONENTS
 
-const roverStats = (data) => {
-    if (!data) { getRover(store) }
-    const {id, landing_date: landingDate, launch_date: launchDate, name, status} = data[store.currentRover].photos[0].rover
-    return `
-        <h3>${name}</h3>
-        <div class="info-card">
-            <ul>
-                <li>Launch date: ${launchDate}</li>
-                <li>Landing date: ${landingDate}</li>
-                <li>Status: ${status}</li>
-            </ul>
-        </div>
-    `
-}
+const roverStats = (data, selectedRover) => {
+  if (data === undefined) {
+    return;
+  }
+  const {
+    landing_date: landingDate,
+    launch_date: launchDate,
+    name,
+    status,
+  } = data[selectedRover].photos[0].rover;
+  return `
+    <h3>${name}</h3>
+    <div class="info-card">
+        <ul>
+            <li>Launch date: ${launchDate}</li>
+            <li>Landing date: ${landingDate}</li>
+            <li>Status: ${status}</li>
+        </ul>
+    </div>
+  `;
+};
 
 // Example of a pure function that renders infomation requested from the backend
-const ImageOfTheDay = (apod) => {
-    // If image does not already exist, or it is not from today -- request it again
-    const today = new Date()
-    const photodate = new Date(apod.date)
+const imageOfTheDay = (apod) => {
+  // If image does not already exist, or it is not from today -- request it again
+  // const today = new Date();
+  // const photodate = new Date(apod.date);
 
-    if (!apod || apod.date === today.getDate() ) {
-        getImageOfTheDay(store)
-    }
+  // if (!apod || apod.date === today.getDate()) {
+  //   getImageOfTheDay((image) => {
+  //     const apod = Immutable.Map({ image });
+  //     updateStore(store, apod);
+  //   });
+  // }
 
-    // check if the photo of the day is actually type video!
-    if (apod.media_type === "video") {
-        return (`
-            <p>See today's featured video <a href="${apod.url}">here</a></p>
-            <p>${apod.title}</p>
-            <p>${apod.explanation}</p>
-        `)
-    } else {
-        return (`
+  // check if the photo of the day is actually type video!
+  // if (apod.media_type === "video") {
+  //   return `
+  //           <p>See today's featured video <a href="${apod.url}">here</a></p>
+  //           <p>${apod.title}</p>
+  //           <p>${apod.explanation}</p>
+  //       `;
+  // } else {
+  if (apod === undefined) {
+    return;
+  }
+  return `
             <img src="${apod.image.url}" height="100%" width="100%" />
-        `)
-    }
-}
+        `;
+};
 
 // ------------------------------------------------------  API CALLS
 
-const getImageOfTheDay = (state) => {
-    let { apod } = state
+const getPhotoHtml = (photoArray) => {
+  return photoArray.map((photo) => {
+    const { earth_date: earthDate, img_src: src, camera } = photo;
+    const html = `
+      <div class="image-card">
+        <img src="${src}" alt="Photo taken by ${photo.rover.name}"/>
+        <ul class="details">
+          <li>Earth date: ${earthDate}</li>
+          <li>Camera: ${camera.name}</li>
+        </ul>
+      </div>
+    `;
+    return html;
+  });
+};
 
-    fetch(`http://localhost:3000/apod`)
-        .then(res => res.json())
-        .then(apod => updateStore(store, { apod }))
+const roverImages = (rover, data) => {
+  if (data === undefined) {
+    return;
+  }
+  return getPhotoHtml(data[rover].photos).join(" ");
+};
 
-    return apod
-}
+const createButtons = (state) => {
+  const rovers = state.get("rovers");
+  const buttonArr = rovers.map((rover) => {
+    return `
+        <button class="update-rover" value="${rover}">${rover}</button>
+      `;
+  });
+  return buttonArr;
+};
 
-const getRover = (state) => {
-    let { roverData } = state
+const handleUpdateRover = (state, rover) => {
+  newRover = rover;
+  const newState = state.set("currentRover", newRover);
+  updateStore(state, newState);
+};
 
-    fetch(`http://localhost:3000/rovers`)
-        .then(res => res.json())
-        .then(roverData => {
-            updateStore(store, { roverData })
-        })
+const buttonListeners = (state) => {
+  const buttons = [...document.querySelectorAll(".update-rover")];
+  buttons.forEach((btn) =>
+    btn.addEventListener("click", (e) => {
+      handleUpdateRover(state, e.target.innerHTML.toLowerCase());
+    })
+  );
+};
 
-    return roverData
-}
+const getImageOfTheDay = (callback) => {
+  fetch("http://localhost:3000/apod")
+    .then((res) => res.json())
+    .then((json) => callback(json));
+};
 
-let button = document.querySelector('.test')
-button.addEventListener('click', () => {
-
-})
+const getRoverData = (callback) => {
+  fetch("http://localhost:3000/rovers")
+    .then((res) => res.json())
+    .then((json) => callback(json));
+};
